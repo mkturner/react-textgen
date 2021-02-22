400ac527ecf342c54923d87507e3f4316bc36419
Marvin T <contact@marvinkturner.com>
2021-02-21 23:59:57 -0500

    feat: add handler for event to form

--- src/App.js
+++ src/App.js
@@ -15,6 +15,19 @@ function App() {
     <section className='section-center'>
       <h3>Tired of boring lorem ipsum?</h3>
       <form onSubmit={handleSubmit} className='lorem-form'></form>
+      <label htmlFor='amount'>Paragraphs</label>
+      {/* Input with onChange handler for event */}
+      <input
+        type='number'
+        name='amount'
+        id='amount'
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+      />
+      {/* Submit button */}
+      <button type='submit' className='btn'>
+        Generate
+      </button>
     </section>
   );
 }
