f42da15481af207d2fafefc2130c4a66aa00131d
Marvin T <contact@marvinkturner.com>
2021-02-24 23:57:42 -0500

    fix: correct form markup

--- src/App.js
+++ src/App.js
@@ -14,20 +14,24 @@ function App() {
   return (
     <section className='section-center'>
       <h3>Tired of boring lorem ipsum?</h3>
-      <form onSubmit={handleSubmit} className='lorem-form'></form>
-      <label htmlFor='amount'>Paragraphs</label>
-      {/* Input with onChange handler for event */}
-      <input
-        type='number'
-        name='amount'
-        id='amount'
-        value={count}
-        onChange={(e) => setCount(e.target.value)}
-      />
-      {/* Submit button */}
-      <button type='submit' className='btn'>
-        Generate
-      </button>
+      <form onSubmit={handleSubmit} className='lorem-form'>
+        <label htmlFor='amount'>Paragraphs</label>
+        {/* Input with onChange handler for event */}
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        {/* Submit button */}
+        <button type='submit' className='btn'>
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptate!</p>
+      </article>
     </section>
   );
 }
